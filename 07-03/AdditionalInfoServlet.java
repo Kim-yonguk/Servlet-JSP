81b409da6f252231ba3832c3906969824e720167
Kim-yonguk <1a1a11a@naver.com>
2017-07-03 22:49:34 +0900

    17-07-03 study
    .

---
+++ 07-03/AdditionalInfoServlet.java
@@ -0,0 +1,30 @@
+package com.edu.test;
+
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.util.*;
+
+@WebServlet("/addInfo")
+
+public class AdditionalInfoServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
+		res.setContentType("text/html;charset=EUC-KR");
+		PrintWriter out=res.getWriter();
+		out.print("<html>");
+		out.print("<head><title>Request 정보출력 Servlet</title></head>");
+		out.print("<body>");
+		out.print("<h6>추가적인 요청 정보</h6>");
+		out.print("Request Method : " +req.getMethod()+"<br/>");
+		out.print("Path info : " +req.getPathInfo()+"<br/>");
+		out.print("Path Translated : " +req.getMethod()+"<br/>");
+		out.print("Query String : " +req.getQueryString()+"<br/>");
+		out.print("Content Length : " +req.getContentLength()+"<br/>");
+		out.print("Content Type : " +req.getContentType()+"<br/>");
+		out.print("</body></html>");
+		out.close();
+	}
+}
---
+++ 07-03/URLInfoServlet.java
@@ -0,0 +1,34 @@
+package com.edu.test;
+
+
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/urlInfo")
+public class URLInfoServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException{
+
+		res.setContentType("text/html;charset=EUC-KR");
+		PrintWriter out=res.getWriter();
+		
+		out.print("<html>");
+		out.print("<head><title>Request 정보 출력 servlet</title></head>");
+		out.print("<body>");
+		out.print("<h3>요청방식과 프로토콜 정보</h3>");
+		out.print("Request URI : " + req.getRequestURI() + "<br/>");
+		out.print("Request URL : " + req.getRequestURL() + "<br/>");
+		out.print("Context Path : " + req.getContextPath() + "<br/>");
+		out.print("Request Protocol : " + req.getProtocol() + "<br/>");
+		out.print("Servlet Path : " + req.getServletPath() + "<br/>");
+		out.print("</body></html>");
+		out.close();
+	
+	
+	
+	
+	}
+
+}
